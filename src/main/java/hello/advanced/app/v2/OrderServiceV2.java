f62674bd9324e83640ba4f464dae8ab797ebf463
junyoung choi <junyeong.b2n2w@gmail.com>
2022-10-11 21:18:30 +0900

    로그추적기 v2 어플리케이션 적용

---
+++ src/main/java/hello/advanced/app/v2/OrderServiceV2.java
@@ -0,0 +1,30 @@
+package hello.advanced.app.v2;
+
+import org.springframework.stereotype.Service;
+
+import hello.advanced.trace.TraceId;
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV2 {
+	private final OrderRepositoryV2 oderRepository;
+	private final HelloTraceV2 trace;
+
+	public void orderItem(TraceId traceId, String itemId) {
+
+		TraceStatus status = null;
+
+		try {
+			status = trace.beginSync(traceId, "OrderService.orderItem()");
+			oderRepository.save(status.getTraceId(), itemId);
+			trace.end(status);
+		} catch (Exception e) {
+			trace.exception(status, e);
+			throw e;
+		}
+
+	}
+}
